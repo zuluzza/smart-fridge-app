78372aa89066e8ab70cc32be1f28c9b8d5b755b1
zuluzza <zuluzza@gmail.com>
2020-09-01 19:25:06 +0300

    Show video on screen
    also some changes to main logic concerning download

--- app/src/main/java/com/example/smartfridgeapp/MainActivity.java
+++ app/src/main/java/com/example/smartfridgeapp/MainActivity.java
@@ -1,55 +1,70 @@
 package com.example.smartfridgeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-
-import com.dropbox.core.DbxException;
-import com.example.smartfridgeapp.DropboxClient;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.VideoView;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
-    static DropboxClient dropboxClient = new DropboxClient();
-
-    String getAppSecretKey()
-    {
-        //TODO
-        return "1234";
-    }
-
-    String getAppKey()
-    {
-        //TODO
-        return "asdf";
-    }
+    private static final String TAG = "SmartFridgeApp_MainActivity";
+    private static final String appFolderPathInDropbox = "smart-fridge";
+    private static String currentVideoFile = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate begin");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //authenticate
-        try {
-            dropboxClient.authDropbox(getAppKey(), getAppSecretKey());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (DbxException e) {
-            e.printStackTrace();
-        }
-        //TODO get latest file name
-        String latestVideoFile = "path_to_it";
-        //TODO download videos
-        try {
-            if (!dropboxClient.downloadFromDropbox(latestVideoFile))
-            {
-                //TODO use latest in local
-            }
-        } catch (DbxException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        PendingIntent pendingResult = createPendingResult(
+                DownloadVideoService.DOWNLOAD_VIDEO_REQUEST_CODE, new Intent(), 0);
+        Intent intent = new Intent(getApplicationContext(), DownloadVideoService.class);
+        startService(intent);
         //TODO show the last
+
+        Log.d(TAG, "onCreate end");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == DownloadVideoService.DOWNLOAD_VIDEO_REQUEST_CODE) {
+            switch (resultCode) {
+                /* POSSIBLE TODOS
+                case DownloadVideoService.AUTHENTICATION_FAILED_CODE:
+                    break;
+                case DownloadVideoService.DOWNLOAD_FAILED_CODE:
+                    break;
+                 */
+                case DownloadVideoService.ERROR_CODE:
+                    Log.e(TAG, "Download failed!");
+                    break;
+                case DownloadVideoService.SUCCESS_CODE:
+                    Log.d(TAG, "succesfully downloaded latest video");
+                    currentVideoFile = DownloadVideoService.getLatestVideoFileName();
+
+                    break;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void startVideoPlayback(View view) {
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView)findViewById(R.id.videoView);
+        //MediaController mediaController = new MediaController(this);
+        // mediaController.setAnchorView(videoView);
+        //videoView.setMediaController(mediaController);
+        Log.d(TAG, "Going to show video from " + currentVideoFile);
+
+        videoView.setVideoPath(currentVideoFile);
+
+        videoView.start();
     }
 }
